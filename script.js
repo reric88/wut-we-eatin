c9afbed9bd2a5f98f2b02148f8cf41dd76000abe
Eric Collard <ruffyreborn@gmail.com>
2023-03-10 08:33:26 -0600

    cleaned up JS, fixed text spacing in CSS, switched some HTML spans on wheel

--- script.js
+++ script.js
@@ -19,33 +19,50 @@ function spin(){
     const element = document.getElementById("main-box");
     let SelectedItem = "";
 
-    let cheesecake = shuffle([1890, 2250, 2610]);
-    let chicfila = shuffle([1850, 2210, 2570]);
+    let cheddars = shuffle([1890, 2250, 2610]);
+
+    // let wendys = shuffle([1750, 2110, 2470]);
+    let where2 = shuffle([1750, 2110, 2470]);
+
     let where = shuffle([1770, 2130, 2490]);
-    let wendys = shuffle([1810, 2170, 2530]);
-    let texas = shuffle([1750, 2110, 2470]);
-    let sbarros = shuffle([1630, 1990, 2350]);
-    let jimmy = shuffle([1570, 1930, 2290]);
-    // let mcds = shuffle([, 2110, 2470]);
-    // let hardees = shuffle([1750, 2110, 2470]);
-    // let cheddars = shuffle([1750, 2110, 2470]);
+
+    let texas = shuffle([1850, 2210, 2570]);
+
+    let chicfila = shuffle([1810, 2170, 2530]);
+
+    let jimmy = shuffle([1630, 1990, 2350]);
+
+    let hardees = shuffle([1570, 1930, 2290]);
+//
+    // let sbarros = shuffle([1500, 1860, 2220]);
+    // let mcds = shuffle([1530, 1890, 2660]);
+    // let cheese = shuffle([1600, 1960, 2320]);
+//
     let results = shuffle([
-        cheesecake[0],
+        cheddars[0],
         chicfila[0],
         where[0],
-        wendys[0],
+        // wendys[0],
+        where2[0],
         texas[0],
-        sbarros[0],
-        jimmy[0]
+        jimmy[0],
+        hardees[0],
+        // mcds[0],
+        // sbarros[0],
+        // cheese[0]
     ]);
 
-    if(cheesecake.includes(results[0])) SelectedItem = "Cheesecake Factory?";
+    if(cheddars.includes(results[0])) SelectedItem = "Cheddars?";
     if(chicfila.includes(results[0])) SelectedItem = "Chic-Fil-A?";
     if(where.includes(results[0])) SelectedItem = "Anywhere is fine.";
-    if(wendys.includes(results[0])) SelectedItem = "Wendy's?";
+    // if(wendys.includes(results[0])) SelectedItem = "Wendy's?";
+    if(where2.includes(results[0])) SelectedItem = "I don't care where we go.";
     if(texas.includes(results[0])) SelectedItem = "Texas Roadhouse?";
-    if(sbarros.includes(results[0])) SelectedItem = "Sbarro's?";
     if(jimmy.includes(results[0])) SelectedItem = "Jimmy John's?";
+    if(hardees.includes(results[0])) SelectedItem = "Hardee's?";
+    // if(mcds.includes(results[0])) SelectedItem = "McDonald's?";
+    // if(sbarros.includes(results[0])) SelectedItem = "Sbarro's?";
+    // if(cheese.includes(results[0])) SelectedItem = "Cheesecake Factory?";
 
     box.style.setProperty("transition", "all ease 5s");
     box.style.transform = "rotate(" + results[0] + "deg)";
@@ -57,12 +74,59 @@ function spin(){
     }, 5000);
 
     setTimeout(function(){
+        if(where.includes(results[0])) //SelectedItem = "Anywhere is fine."; 
+        {
+            aww.play();
+            Swal.fire(
+                SelectedItem,
+                'You choose.'
+            )
+        } else if(where2.includes(results[0])) //SelectedItem = "Anywhere is fine."; 
+        {
+            aww.play();
+            Swal.fire(
+                SelectedItem,
+                'I want you to pick.'
+            )
+        } else {
         aww.play();
         Swal.fire(
-            SelectedItem,
-            'I dont really want ' + SelectedItem + '...',
-          )
+                SelectedItem,
+                'I dont really want ' + SelectedItem + '...'
+            
+        )
+    }
     }, 5500)
+
+
+//     if (SelectedItem == where){
+
+//     setTimeout(function(){
+//         aww.play();
+//         Swal.fire(
+//             'Anywhere is fine.',
+//             'You choose.'
+//           )
+//     }, 5500)
+// } else {
+    
+//     setTimeout(function(){
+//         aww.play();
+//         Swal.fire(    
+//         SelectedItem,
+//         'I dont really want ' + SelectedItem + '...'
+//           )
+//     }, 5500)
+    
+    
+//     setTimeout(function(){
+//         aww.play();
+//         Swal.fire(    
+//         SelectedItem,
+//         'I dont really want ' + SelectedItem + '...'
+//           )
+//     }, 5500)
+// }
 
     setTimeout(function(){
         box.style.setProperty("transition", "initial");
